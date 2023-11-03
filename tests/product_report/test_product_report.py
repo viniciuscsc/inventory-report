afbd9a5a089e5aa3d582a18cf01825cac272f3c6
vcscunha <vcs.cunha@icloud.com>
2023-11-03 16:42:38 -0300

    Req 02: implementa teste para validar o método mágico "str" de uma instância da classe Product

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,23 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    prod_teste = Product(
+        "001",
+        "produto",
+        "empresa",
+        "fabricacao",
+        "validade",
+        "num_serie",
+        "armazenar",
+    )
+
+    assert prod_teste.__str__() == (
+        f"The product {prod_teste.id} - {prod_teste.product_name} "
+        f"with serial number {prod_teste.serial_number} "
+        f"manufactured on {prod_teste.manufacturing_date} "
+        f"by the company {prod_teste.company_name} "
+        f"valid until {prod_teste.expiration_date} "
+        "must be stored according to the following instructions: "
+        f"{prod_teste.storage_instructions}."
+    )
