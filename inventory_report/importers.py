5e31438d3dda65688cee681de14b54b68161e973
vcscunha <vcs.cunha@icloud.com>
2023-11-03 17:11:07 -0300

    Req 04, parte 01: implementa método import_data() da classe JsonImporter, sem considerar as Excessões

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,6 +1,7 @@
 from typing import Dict, Type
 from abc import ABC, abstractmethod
 from inventory_report.product import Product
+import json
 
 
 class Importer(ABC):
@@ -12,8 +13,27 @@ class Importer(ABC):
         pass
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self) -> list[Product]:
+        try:
+            with open(self.path, "r") as inventario_json:
+                dados_json = json.load(inventario_json)
+                produtos = []
+
+                for produto_dict in dados_json:
+                    produto = Product(
+                        produto_dict["id"],
+                        produto_dict["product_name"],
+                        produto_dict["company_name"],
+                        produto_dict["manufacturing_date"],
+                        produto_dict["expiration_date"],
+                        produto_dict["serial_number"],
+                        produto_dict["storage_instructions"],
+                    )
+
+                    produtos.append(produto)
+                
+                return produtos
 
 
 class CsvImporter:
