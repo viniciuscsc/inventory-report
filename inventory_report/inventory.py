824e93d23d155fd6272afb0ad0aa587f82c92013
vcscunha <vcs.cunha@icloud.com>
2023-11-04 17:27:26 -0300

    Req 05: implementa a classe Inventory

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,15 @@
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: list[Product] | None = None):
+        if data is None:
+            data = []
+        self._data = data
+
+    @property
+    def data(self):
+        return self._data
+
+    def add_data(self, data: list[Product]) -> None:
+        self._data.extend(data)
