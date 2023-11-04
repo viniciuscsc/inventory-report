fc13615fa34f7b511c030abbccb3bd4412320f27
vcscunha <vcs.cunha@icloud.com>
2023-11-04 17:39:22 -0300

    Req 06: implementa o protocolo Report

--- inventory_report/reports/report.py
+++ inventory_report/reports/report.py
@@ -1,2 +1,10 @@
-class Report:
-    pass
+from inventory_report.inventory import Inventory
+from typing import Protocol
+
+
+class Report(Protocol):
+    def add_inventory(self, inventory: Inventory) -> None:
+        pass
+
+    def generate(self) -> str:
+        pass
